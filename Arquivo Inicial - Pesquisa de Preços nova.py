dcef23861b1c322ac412fc6bf4c79134056727cf
Lucas Maciel <lucasboni_business@outlook.com>
2024-09-26 21:11:17 -0300

    Update Arquivo Inicial - Pesquisa de Preços nova.py

--- Arquivo Inicial - Pesquisa de Preços nova.py
+++ Arquivo Inicial - Pesquisa de Preços nova.py
@@ -196,7 +196,7 @@ if len(df_ofertas.index) > 0:
     <p>Encontramos alguns produtos em oferta dentro da faixa de preço desejada. Segue tabela com detalhes</p>
     {df_ofertas.to_html(index=False)}
     <p>Qualquer dúvida estou à disposição</p>
-    <p>Att., Lukas Maciel</p>
+    <p>Att., Lucas Boni</p>
     """
     
     mail.Send()
